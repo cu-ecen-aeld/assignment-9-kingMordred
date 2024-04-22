0db744402bcfbc7382efe17f55b12bb37ec3f3b3
Ayoub Ayari <ayoub.ayari_ext@softathome.com>
2024-04-22 16:47:19 +0100

    assignment-7-part-2-complete

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -11,9 +11,17 @@ LDD_VERSION = 'ab0c850'
 # Your site should start with git@github.com:
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-kingMordred.git'
 LDD_SITE_METHOD = git
-#LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules/
+LDD_MODULE_SUBDIRS += scull/
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
