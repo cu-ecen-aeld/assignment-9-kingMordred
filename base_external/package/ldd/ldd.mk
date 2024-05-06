f3195405afd1aeae86e914a7abce130c4e7f0245
Ayoub Ayari <ayoub.ayari_ext@softathome.com>
2024-05-06 13:28:40 +0100

    assignment-7-part-2-complete

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,21 +5,22 @@
 #
 ##############################################################
 
-LDD_VERSION = 'ab0c850'
+#Fill up the contents below in order to reference your assignment 7 git contents
+LDD_VERSION = 'f8cc5412f48fa259e325f438898ece9cc88950c3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-kingMordred.git'
 LDD_SITE_METHOD = git
-
 LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = misc-modules/
-LDD_MODULE_SUBDIRS += scull/
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load   $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 endef
 
