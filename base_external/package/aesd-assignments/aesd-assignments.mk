ea72cfddf2a3437c8ad85b7cfe8b9578149f0803
Ayoub Ayari <ayoub.ayari_ext@softathome.com>
2024-05-20 17:09:59 +0100

    assignment-9-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd17175a39e036eb3312aa933dbe738d2bb0d8208'
+AESD_ASSIGNMENTS_VERSION = 'e5051a6c404a470393a45e22ed0baaa09da7af25'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
